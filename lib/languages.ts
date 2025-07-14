44cff22e2b4b4dc6577efe3f3f3093e89bf973c5
jian bin hao <1541373309@qq.com>
2025-07-14 23:14:30 +0800

    feat(opton):抽象language文件

---
+++ lib/languages.ts
@@ -0,0 +1,17 @@
+export const LANGUAGES = [
+  { label: '中文（简体）', value: 'zh-CN' },
+  { label: '中文（繁体）', value: 'zh-TW' },
+  { label: '英语', value: 'en' },
+  { label: '日语', value: 'ja' },
+  { label: '韩语', value: 'ko' },
+  { label: '法语', value: 'fr' },
+  { label: '德语', value: 'de' },
+  { label: '西班牙语', value: 'es' },
+  { label: '俄语', value: 'ru' },
+  { label: '葡萄牙语', value: 'pt' },
+];
+
+export const UI_LANGUAGES = [
+  { label: '跟随浏览器', value: 'default' },
+  ...LANGUAGES
+]; 
